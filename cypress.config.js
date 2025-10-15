d54cb1a8d01bfd145a181741a6c33aa7029704b6
Friyn <friyn@users.noreply.github.com>
2025-10-15 12:44:50 +0700

    Fixing loginEveryRole

--- cypress.config.js
+++ cypress.config.js
@@ -3,16 +3,21 @@ require('dotenv').config();
 
 module.exports = defineConfig({
   e2e: {
-    baseUrl: process.env.BASE_URL,
+    // baseUrl: process.env.BASE_URL,
     env: {
+      baseUrl: process.env.BASE_URL,
       adminEmail: process.env.ADMIN_EMAIL,
       adminPassword: process.env.ADMIN_PASSWORD,
+      adminUrl: process.env.ADMIN_URL,
       studentEmail: process.env.STUDENT_EMAIL,
       studentPassword: process.env.STUDENT_PASSWORD,
+      studentUrl: process.env.STUDENT_URL,
       teacherEmail: process.env.TEACHER_EMAIL,
       teacherPassword: process.env.TEACHER_PASSWORD,
+      teacherUrl: process.env.TEACHER_URL,
       staffEmail: process.env.STAFF_EMAIL,
       staffPassword: process.env.STAFF_PASSWORD,
+      staffUrl: process.env.STAFF_URL,
     },
   },
 });
